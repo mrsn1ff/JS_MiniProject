4b15272f853ac1755293213171857466612d09a8
sn1ff <119067155+mrsn1ff@users.noreply.github.com>
2024-07-11 18:39:52 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,25 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+	let item = document.createElement("li");
+	item.innerText = inp.value;
+	
+	let delBtn = document.createElement("button");
+	delBtn.innerText = "delete";
+	delBtn.classList.add("delete");
+	
+	item.appendChild(delBtn);
+	ul.appendChild(item);
+	inp.value = "";
+});
+
+ul.addEventListener("click" , function(event){
+	if(event.target.nodeName == "BUTTON"){
+		let listItem = event.target.parentElement;
+		listItem.remove();
+	}
+})
+
+
